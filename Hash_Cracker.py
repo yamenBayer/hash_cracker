29c340b9eef7dfa1330c7639cb30c7612dce369a
Yamen Bayer <yamen.warsss@gmail.com>
2023-05-18 17:58:51 +0400

    INIT

---
+++ Hash_Cracker.py
@@ -0,0 +1,202 @@
+import hashlib
+import itertools
+import os
+import threading
+import time
+import argparse
+
+#####################################################################################################
+########################################## Global Variables #########################################
+#####################################################################################################
+wordlist = []
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~'
+flag = False
+lengths = []
+threads = []
+speed = 1
+cores_used = 1
+#####################################################################################################
+#####################################################################################################
+#####################################################################################################
+
+#####################################################################################################
+############################################# Functions #############################################
+#####################################################################################################
+
+class ThreadWithResult(threading.Thread):
+    def __init__(self, target, args=()):
+        super().__init__(target=target, args=args)
+        self._result = None
+
+    def run(self):
+        self._result = self._target(*self._args)
+
+    def result(self):
+        return self._result
+    
+def get_lengths(length, cores):
+    global lengths
+    lengths = []
+    for n in range(0, cores):
+        if n == cores-1 and (length - ((int(length/cores))*cores)) > 0:
+            value = int(length/cores) + (length - ((int(length/cores))*cores)) + lengths[n-1]
+        elif n!=0: 
+            value = int(length/cores) + lengths[n-1]
+        else:
+            value = int(length/cores)
+
+        lengths.append(value) 
+
+def generate_wordlist(length):
+    global wordlist
+    
+
+    combinations = itertools.product(alphabet, repeat=length)
+    for combination in combinations:
+        word = ''.join(combination)
+        wordlist.append(word)
+
+
+def process(len1, len2, hash):
+    total_words_checked = 0  # Counter for the number of words checked
+    duration_threshold = 1.0
+
+    for i in range(len1, len2):
+        total_words_checked += 1
+        
+        global flag
+        if flag == False:
+            elapsed_time = time.time() - start_time
+            if elapsed_time >= duration_threshold :
+                global speed
+                speed = total_words_checked
+                flag = True
+
+        word = wordlist[i]
+        hashes = []
+        
+        hashes.extend([hashlib.md5(word.encode()).hexdigest(), hashlib.sha1(word.encode()).hexdigest(), hashlib.sha224(word.encode()).hexdigest(), hashlib.sha256(word.encode()).hexdigest(), hashlib.sha384(word.encode()).hexdigest(), hashlib.sha512(word.encode()).hexdigest()])
+        if hash in hashes:
+            return word
+        
+        
+        # Display the counter dynamically
+        print(f"Words checked: {total_words_checked}", end="\r", flush=True)
+
+    return None
+
+def crack_hash(hash_to_crack, max_length, cores_num):
+    
+
+    for length in range(1, max_length+1):
+        generate_wordlist(length)
+        print("Iteration "+ str(length) + " processing...")
+        words_len = len(wordlist)
+
+        get_lengths(words_len, cores_num)
+
+        global threads
+        threads = []
+        if length > 3:
+
+            old_min = 0
+            for n in range(0, cores_num):
+                t = ThreadWithResult(target=process, args = (old_min, lengths[n], hash_to_crack))
+                old_min = lengths[n]
+                threads.append(t)
+                t.start()
+
+        else:
+            t = ThreadWithResult(target=process, args = (0, words_len, hash_to_crack))
+            threads.append(t)
+            t.start()
+            
+        for t in threads:
+            t.join()
+
+        for t in threads:
+            if t.result() is not None:
+                return t.result()
+
+    return None
+#####################################################################################################
+#####################################################################################################
+#####################################################################################################
+
+
+#####################################################################################################
+############################################### Main ################################################
+#####################################################################################################
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Hash Cracker')
+    parser.add_argument('-ha', '--hash', type=str, help='Hash to crack')
+    parser.add_argument('-c', '--cores', type=int, default=1, help='Number of processor cores to use')
+    parser.add_argument('-th', '--to-hash', type=str, help='Word to hash')
+    parser.add_argument('-m', '--method', choices=['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512'], default='md5', help='Hashing algorithm method to use')
+    args = parser.parse_args()
+
+    if args.to_hash:
+        word = args.to_hash
+        method = args.method.lower()
+        if method == 'md5':
+            hash_value = hashlib.md5(word.encode()).hexdigest()
+        elif method == 'sha1':
+            hash_value = hashlib.sha1(word.encode()).hexdigest()
+        elif method == 'sha224':
+            hash_value = hashlib.sha224(word.encode()).hexdigest()
+        elif method == 'sha256':
+            hash_value = hashlib.sha256(word.encode()).hexdigest()
+        elif method == 'sha384':
+            hash_value = hashlib.sha384(word.encode()).hexdigest()
+        elif method == 'sha512':
+            hash_value = hashlib.sha512(word.encode()).hexdigest()
+        else:
+            print('Invalid hashing algorithm')
+            exit(1)
+
+        print(f"{method.upper()} hash for '{word}': {hash_value}")
+
+    else:
+        hash = args.hash
+        max_length = 4  # Maximum word length to try
+        cores_num = args.cores
+
+        num_cores = os.cpu_count()
+        print("-----------------------------------------------")
+        print("Number of available processor cores:", num_cores)
+        if (cores_num > num_cores):
+            avb_cores = num_cores
+            print("Number of processor cores used by this code:", avb_cores)
+        elif (cores_num < 0):
+            avb_cores = 1
+            print("Number of processor cores used by this code:", avb_cores)
+        else:
+            avb_cores = int(cores_num)
+            print("Number of processor cores used by this code:", avb_cores)
+        print("-----------------------------------------------\n")
+        start_time = time.time()
+        cracked_password = crack_hash(hash, max_length, cores_num)
+
+        if cracked_password:
+            print("\n")
+            print(f"-> Hash cracked! The orginal word is: {cracked_password}")
+        else:
+            print("\nPassword not found in the wordlist.")
+
+        end_time = time.time()
+        # Calculate the elapsed time
+        elapsed_time = end_time - start_time
+        hours = int(elapsed_time // 3600)
+        minutes = int((elapsed_time % 3600) // 60)
+        seconds = int(elapsed_time % 60)
+        # Format the elapsed time as hours:minutes:seconds
+        elapsed_time_formatted = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+        print("\n-----------------------------------------------")
+        print("Elapsed time:", elapsed_time_formatted)
+        print("Code Speed:", speed, "words per second")
+        print("-----------------------------------------------")
+
+#####################################################################################################
+#####################################################################################################
+#####################################################################################################
---
+++ README.md
@@ -0,0 +1,70 @@
+<h1 align="center">Hash Cracker</h1>
+<p align="center">
+    <a href="https://python.org">
+    <img src="https://img.shields.io/badge/Python-3.7-green.svg">
+  </a>
+  <a href="https://github.com/PushpenderIndia/technowhorse/blob/master/LICENSE">
+    <img src="https://img.shields.io/badge/License-BSD%203-lightgrey.svg">
+  </a>
+  <a href="https://github.com/PushpenderIndia/technowhorse/releases">
+    <img src="https://img.shields.io/badge/Release-1.0-blue.svg">
+  </a>
+    <a href="https://github.com/PushpenderIndia/technowhorse">
+    <img src="https://img.shields.io/badge/Open%20Source-%E2%9D%A4-brightgreen.svg">
+  </a>
+</p>
+
+
+<p align="center">
+  Advanced hash Cracker/Decoder/Decryptor written in Python 3
+</p>
+
+
+## Disclaimer
+<p align="center">
+  This cracker was created only for white hackers, the 
+</p>
+
+THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND. YOU MAY USE THIS SOFTWARE AT YOUR OWN RISK. THE USE IS COMPLETE RESPONSIBILITY OF THE END-USER. THE DEVELOPERS ASSUME NO LIABILITY AND ARE NOT RESPONSIBLE FOR ANY MISUSE OR DAMAGE CAUSED BY THIS PROGRAM.
+
+## Features
+- [*] Works on Windows/Linux/OSx etc which supports Python 3
+- [*] Advanced, Lightweight, Easy to use
+- [*] Based on Advanced-BruteForce Technique
+- [*] Shows total attempts made by Cracker
+- [*] Automatically Stops the Script When Correct Password is Found
+
+## Prerequisite
+- [x] Python 3.X  
+
+## How To Use
+```bash
+# Install dependencies 
+$ pip install latest python 3.x
+
+# Clone this repository
+$ git clone https://github.com/yamenBayer/hash_cracker.git
+
+# Go into the repository
+$ cd hash_cracker
+
+# Getting Help Menu
+$ python hash_cracker.py --help
+
+# Cracking MD5 Hash Without saving Result
+$ python hash_cracker.py -ha <Hash> -c <no. of cores>
+$ Note: no. of cores must be equal or lower than the available cores in your system.
+
+# Cracking Example
+$ python hash_cracker.py -ha 22c2e450a8f0933878bf16a55e3960c69ff4c76684bb67fb553497eababd9e4f61577331480dd4af6b905f90df6e6db4d4b364ae262cb9b9ec303f8086249f85 -c 6
+```
+
+### Note : Procedure for cracking is same in all OS, and the maximum time around half hour and maximum word length supported is 4
+
+
+## Contributors:
+All contributor's pull request will be accepted if their pull request is worthy for this repo.
+
+
+## Contact 
+yamen.warss@gmail.com
